233cdab0ad1e0304b04bd67bd5db3db9444a6e30
Alikhawari <56734151+Alikh-2617@users.noreply.github.com>
2020-11-03 13:19:18 +0100

    ny cod

--- src/fileCrawler/FileCrawler.java
+++ src/fileCrawler/FileCrawler.java
@@ -4,45 +4,44 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class FileCrawler {
+
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\alikh\\IdeaProjects\\classUppgift\\src\\file");
-        Scanner sc3 = new Scanner(System.in);
+        File filePath = new File("./");
         System.out.println("Skriv ett ord att söka : ");
-        String name = sc3.nextLine();
+        Scanner sc2 = new Scanner(System.in);
+        String searchName = sc2.nextLine();
+
         try
         {
-            serch_Directory(file, name);
+            searchDirectory(filePath, searchName);
         }
         catch ( IOException e )
         {
             System.out.println(e.getMessage());
         }
-
-
     }
 
-    static void serch_Directory (File file , String name ) throws IOException
+    static void searchDirectory (File file , String searchName ) throws IOException
     {
         if (file.isDirectory()) {
             File[] folder = file.listFiles();
             for (File files : folder) {
-                serch_File(files, name);
+                searchDirectory(files, searchName);
             }
         }
-    }
-    static void serch_File (File file , String name) throws IOException
-    {
-        Scanner sc = new Scanner(file);
-        if (file.isFile()) {
+
+        else if (file.isFile()) {
+            Scanner sc = new Scanner(file);
             while (sc.hasNext()) {
-                String sercher = name;
-                if (sercher.equalsIgnoreCase(name)) {
+                String name = searchName;
+                if (name.equalsIgnoreCase(searchName)) {
                     System.out.println("ordet finns i en file med den sökväg :" + file.getAbsolutePath());
-                    System.out.println("--------------------------------------");
+                    System.out.println("-------------------------------------");
                 }
+                break;
             }
-            sc.close();
-        } else if (!file.canRead()) {
+        }
+        else if (!file.canRead()) {
             System.out.println("Filen gå inte att läsas :" + file.getAbsolutePath());
         }
 
